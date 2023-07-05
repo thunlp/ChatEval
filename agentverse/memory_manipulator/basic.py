c64cdb66b9fbacf5ec3fa6fea4663f8e6ab0e511
dalabengba <zorowin123@gmail.com>
2023-07-05 12:31:12 +0800

    temp

--- agentverse/memory_manipulator/basic.py
+++ agentverse/memory_manipulator/basic.py
@@ -3,8 +3,11 @@ from typing import Dict, List
 
 from pydantic import BaseModel, Field
 
+from . import memory_manipulator_registry
+
 from agentverse.message import Message
 from agentverse.memory_manipulator import BaseMemoryManipulator
+
 
 @memory_manipulator_registry.register("basic")
 class BasicMemoryManipulator(BaseMemoryManipulator):
