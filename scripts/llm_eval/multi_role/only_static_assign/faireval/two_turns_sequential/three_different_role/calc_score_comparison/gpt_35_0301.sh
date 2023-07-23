16a63e63aeaec8aa2a38ad050cf07ec278c6989c
dalabengba <zorowin123@gmail.com>
2023-07-23 23:33:26 +0800

    fix scripts bug

--- scripts/llm_eval/multi_role/only_static_assign/faireval/two_turns_sequential/three_different_role/calc_score_comparison/gpt_35_0301.sh
+++ scripts/llm_eval/multi_role/only_static_assign/faireval/two_turns_sequential/three_different_role/calc_score_comparison/gpt_35_0301.sh
@@ -7,6 +7,6 @@ export OPENAI_API_KEY='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'
 
 python llm_eval.py \
 --task "llm_eval/multi_role/only_static_assign/faireval/two_turns_sequential/three_different_role/calc_score_comparison/gpt_35_0301" \
---data_path "./agentverse/tasks/llm_eval/data/nlg_eval/preprocessed_data/test.json" \
+--data_path "./agentverse/tasks/llm_eval/data/faireval/preprocessed_data/test.json" \
 --output_dir "./outputs/llm_eval/multi_role/only_static_assign/faireval/two_turns_sequential/three_different_role/calc_score_comparison/gpt_35_0301" \
 #--reverse_input true
